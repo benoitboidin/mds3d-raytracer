9ba44b5a101c8895efed64039c6535dcc487417d
Boidin Benoît <benoit.bboidin@gmail.com>
2023-01-16 19:54:38 +0100

    Petanque works but weirdly

--- src/shapes/plane.cpp
+++ src/shapes/plane.cpp
@@ -8,7 +8,25 @@ Plane::Plane(const PropertyList &propList) {
 
 void Plane::intersect(const Ray &ray, Hit &hit) const {
   /// TODO: compute ray-plane intersection
-  throw RTException("Plane::intersect not implemented yet.");
+  // throw RTException("Plane::intersect not implemented yet.");
+
+  Point3f a = m_position;
+  Vector3f d = ray.direction;
+  Vector3f n = m_normal;
+  Point3f o = ray.origin;
+
+  float equation = (o.dot(n) + a.dot(n)) + hit.t*(d.dot(n));
+
+  if (d.dot(n) == 0) {
+    hit.t = 0;
+    hit.shape = this;
+  } else if (d.dot(n) > 0) {
+    hit.t = equation;
+    hit.shape = this;
+    hit.normal = m_normal;
+  } else {
+    hit = Hit();
+  }
 }
 
 REGISTER_CLASS(Plane, "plane")
